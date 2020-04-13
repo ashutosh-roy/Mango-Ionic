766d24fc2483292df0b62b45e571938daaa327cd
dopecoder007 <dopecoder007@gmail.com>
2020-04-14 00:41:43 +0530

    Added HOME & INDEX folders to store unauthenticated & the authenticated pages of our app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,42 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'index',
+    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
+  },
+  {
+    path: 'feed',
+    loadChildren: () => import('./page/feed/feed.module').then( m => m.FeedPageModule)
+  },
+  {
+    path: 'messages',
+    loadChildren: () => import('./page/messages/messages.module').then( m => m.MessagesPageModule)
+  },
+  {
+    path: 'notifications',
+    loadChildren: () => import('./page/notifications/notifications.module').then( m => m.NotificationsPageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('./page/settings/settings.module').then( m => m.SettingsPageModule)
+  },
+  {
+    path: 'welcome',
+    loadChildren: () => import('./page/welcome/welcome.module').then( m => m.WelcomePageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./page/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./page/signup/signup.module').then( m => m.SignupPageModule)
   }
 ];
 @NgModule({
