44f3fc4f683a773c1526d0e799cd6fe01b8f4117
dopecoder007 <dopecoder007@gmail.com>
2020-04-16 21:47:54 +0530

    Login & SignUp Funcationality Working With the validations.

--- src/app/components/slides/slides.component.ts
+++ src/app/components/slides/slides.component.ts
@@ -9,7 +9,7 @@ export class SlidesComponent implements OnInit
 {
   // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
   slideOpts = {
-    initialSlide: 1,
+    initialSlide: 0,
     speed: 400
   };
   constructor() { }
